118e49e1e95d862306144ce1cd51a85d7ec663d6
Ludwilton <ludwig.carlegrund@gmail.com>
2025-01-21 10:53:38 +0100

    linearregression.py shell

---
+++ linearregression.py
@@ -0,0 +1,52 @@
+import numpy as np
+import scipy.stats as stats
+
+class LinearRegression:
+    def __init__(self):
+        pass
+
+    
+    # property d that contains the number of features/parameters/dimensions of the model.
+
+    
+    # property n that contains the size of the sample.
+
+
+    # property confidence_level that stores the selected confidence level.
+
+
+    def calculate_variance(self, x):
+        # Calculate the variance of the x array
+        pass
+
+
+    def calculate_standard_deviation():
+        pass
+
+
+    def regression_significance(self):
+        # Calculate the significance of the regression
+        pass
+
+
+    def regression_relevance(self):
+        # Calculate the relevance of the regression
+        pass
+
+    
+    def variable_significance(self):
+        # Calculate the significance of each variable
+        pass
+
+
+    def pearson_correlation(self):
+        # Calculate the Pearson correlation coefficient
+        pass
+
+
+    def confidence_interval(self):
+        # Calculate the confidence interval
+        pass
+
+
+    
